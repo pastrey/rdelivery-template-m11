5c3afe3bc72fc6c9f4228e781bfcbdf4818b1d5e
Francis Patry-Jessop <fjessop@gmail.com>
2023-08-29 11:38:22 -0600

    remove unused dependencies

--- src/main/java/com/rocketFoodDelivery/rocketFood/models/UserEntity.java
+++ src/main/java/com/rocketFoodDelivery/rocketFood/models/UserEntity.java
@@ -1,6 +1,5 @@
 package com.rocketFoodDelivery.rocketFood.models;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
--- src/main/java/com/rocketFoodDelivery/rocketFood/service/AddressService.java
+++ src/main/java/com/rocketFoodDelivery/rocketFood/service/AddressService.java
@@ -5,7 +5,6 @@ import com.rocketFoodDelivery.rocketFood.repository.AddressRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Optional;
 
 
